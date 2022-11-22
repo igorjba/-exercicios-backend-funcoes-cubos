cfd16431ab72af6d8c00a43777b54c0ed094bbfa
igorjba <igorjbahia@gmail.com>
2022-11-22 05:48:26 -0300

    :sparkles: feat: Exercício 01 resolvido.

--- 01/index.js
+++ 01/index.js
@@ -26,3 +26,15 @@ const prova = {
     ]
 };
 
+function corrigirprova(provaAluno) {
+    let acertos = 0;
+
+    for (let i = 0; i < prova.questoes.length; i++) {
+        if (prova.questoes[i].resposta === prova.questoes[i].correta) {
+            acertos = acertos + 1;
+        }
+    }
+    let pontos = acertos * 2;
+    console.log(`O aluno(a) João acertou ${acertos} questões e obteve nota ${pontos}`)
+}
+corrigirprova(prova);
