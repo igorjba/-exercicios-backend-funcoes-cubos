1cd157c5e17e8b87d726e6e6685ba547c3e9e1a5
igorjba <igorjbahia@gmail.com>
2022-12-07 14:16:47 -0300

    :recycle: refactor: Acrescentadas as respostas das alternativas E e F, além das questões extras.

--- 02/index.js
+++ 02/index.js
@@ -1,30 +1,60 @@
 const carro = {
     ligado: false,
     velocidade: 0,
+    devolveStatusTexto: function () {
+        return this.ligado ? "ligado" : "desligado";
+    },
     ligar: function () {
         if (this.ligado === true) {
-            console.log("Este carro está ligado");
+            console.log("Este carro está ligado.");
+        } else {
+            this.ligado = true;
+            this.imprimeStatusEVelocidade();
         }
     },
     desligar: function () {
         if (this.ligado === false) {
             console.log("Este carro já está desligado.");
+        } else if (this.ligado === true && this.velocidade === 0) {
+            this.ligado = false;
+            this.velocidade = 0;
+            this.imprimeStatusEVelocidade();
+        } else {
+            console.log("Não é possível desligar um carro em movimento.")
         }
     },
     acelerar: function () {
         if (this.ligado === false) {
-            console.log("Não é possível acelerar um carro desligado.");
-        } else {
-            this.velocidade = this.velocidade + 10;
+            this.ligado = true;
         }
+
+        this.velocidade += 10;
+        this.imprimeStatusEVelocidade();
     },
     desacelerar: function () {
         if (this.ligado === false) {
             console.log("Não é possível desacelerar um carro desligado.");
+        } else if (this.velocidade === 0) {
+            this.ligado = false;
         } else {
             this.velocidade = this.velocidade - 10;
+            if (this.velocidade === 0) {
+                this.ligado = false;
+            }
+            this.imprimeStatusEVelocidade();
         }
+    },
+    imprimeStatusEVelocidade: function () {
+        console.log(`Carro ${this.devolveStatusTexto()}. Velocidade: ${this.velocidade}.`);
     }
 }
 
-
+carro.desligar();
+carro.ligar();
+carro.ligar();
+carro.acelerar();
+carro.acelerar();
+carro.desacelerar();
+carro.desligar();
+carro.acelerar();
+carro.desacelerar();
